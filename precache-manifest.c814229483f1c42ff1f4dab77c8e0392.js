ed81a705b266e587447b4854bef742be6d7ea480
FinancialBlock <financialblockchain@gmail.com>
2022-01-11 21:38:45 -0600

    Updates

---
+++ precache-manifest.c814229483f1c42ff1f4dab77c8e0392.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89d6c8c425ca369e82b609b1b9cce93",
+    "url": "/FAC-Dex/index.html"
+  },
+  {
+    "revision": "22c0ce2895a4c83cabad",
+    "url": "/FAC-Dex/static/css/2.3a0ac4c9.chunk.css"
+  },
+  {
+    "revision": "7145ea945d2237b926aa",
+    "url": "/FAC-Dex/static/css/main.99ede3ff.chunk.css"
+  },
+  {
+    "revision": "22c0ce2895a4c83cabad",
+    "url": "/FAC-Dex/static/js/2.89f66984.chunk.js"
+  },
+  {
+    "revision": "70fd27d7534104d11b965a74ff67c29f",
+    "url": "/FAC-Dex/static/js/2.89f66984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7145ea945d2237b926aa",
+    "url": "/FAC-Dex/static/js/main.bae34324.chunk.js"
+  },
+  {
+    "revision": "29e21931f35cb4501118",
+    "url": "/FAC-Dex/static/js/runtime-main.884db402.js"
+  },
+  {
+    "revision": "98eafb0aa0793c731ee00051b38cedda",
+    "url": "/FAC-Dex/static/media/Artboard 1.98eafb0a.png"
+  }
+]);
